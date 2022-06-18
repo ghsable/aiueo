764bf267e5892e1f3b89146425723e7922f7494c
dativus <sn.sable005@gmail.com>
2022-06-18 18:12:48 +0900

    update App.tsx - remove <p>

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <CustomizedInputBase />
         <Stack spacing={2}>
           <Button variant="contained">Hello World</Button>
