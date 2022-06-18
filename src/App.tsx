2add9062a7e23ed8184ae93b8fcf4dfea51353b8
dativus <sn.sable005@gmail.com>
2022-06-18 17:53:01 +0900

    add FullWidthTextField

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import StandardImageList from './StandardImageList';
+import FullWidthTextField from './FullWidthTextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
@@ -13,6 +14,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <FullWidthTextField />
         <Stack spacing={2}>
           <Button variant="contained">Hello World</Button>
         </Stack>
