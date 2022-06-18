c3db7bb206cf88d2d7cceb72cedc20a7acf83985
dativus <sn.sable005@gmail.com>
2022-06-18 22:47:12 +0900

    update App.tsx - remove stack, button

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import StandardImageList from './StandardImageList';
 import CustomizedInputBase from './CustomizedInputBase';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
 
 function App() {
   return (
@@ -12,9 +10,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <CustomizedInputBase />
-        <Stack spacing={2}>
-          <Button variant="contained">Hello World</Button>
-        </Stack>
         <StandardImageList />
         <a
           className="App-link"
