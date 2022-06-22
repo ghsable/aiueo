284848bb5721a69e500173fba038ef7c140637bd
dativus <sn.sable005@gmail.com>
2022-06-22 14:57:53 +0900

    update SearchBar.tsx - remove MenuIcon

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 
 export default function SearchBar() {
@@ -11,9 +10,6 @@ export default function SearchBar() {
       component="form"
       sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
     >
-      <IconButton sx={{ p: '10px' }} aria-label="menu">
-        <MenuIcon />
-      </IconButton>
       <InputBase
         sx={{ ml: 1, flex: 1 }}
         placeholder="... φ(．．)"
