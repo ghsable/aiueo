b9889acaa5d2fd75bf29088a2f31faa91d098a15
dativus <sn.sable005@gmail.com>
2022-06-22 16:26:56 +0900

    update App.tsx - update StandardImageList to ProductTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import SearchBar from './SearchBar';
-import StandardImageList from './StandardImageList';
+import ProductTable from './ProductTable';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <SearchBar />
       </header>
-        <StandardImageList />
+        <ProductTable />
     </div>
   );
 }
