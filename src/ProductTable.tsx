402bd4e7ec9a696b336600b077001c2bdf75fded
dativus <sn.sable005@gmail.com>
2022-06-22 16:24:32 +0900

    add ProductTable.tsx

---
+++ src/ProductTable.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StandardImageList from './StandardImageList';
+
+function ProductTable() {
+  return (
+    <div className="ProductTable">
+      <StandardImageList />
+    </div>
+  );
+}
+
+export default ProductTable;
