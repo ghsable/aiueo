1c87c9aa2cf20cf9a4c680257ca78fce331aa5ec
dativus <sn.sable005@gmail.com>
2022-07-02 21:54:29 +0900

    update ProductTable.tsx - add filterProducts

--- src/ProductTable.tsx
+++ src/ProductTable.tsx
@@ -3,18 +3,23 @@ import ProductValue from './ProductValue';
 import ImageList from '@mui/material/ImageList';
 
 function ProductTable({ products, filterText }: { products: any , filterText: string }) {
+  const filterProducts: any[] = [];
+  products.map((product: any, i: number) => {
+    if (product.title.indexOf(filterText) === -1) {
+      return;
+    }
+    filterProducts.push(
+      <ProductValue
+        product={product}
+        key={i}
+      />
+    );
+  });
+
   return (
     <div className="ProductTable">
       <ImageList sx={{ width: 1000, height: 850 }} cols={7} rowHeight={230}>
-        {products.map((product: any, i: number) => {
-          if (product.title.indexOf(filterText) === -1) {
-            return;
-          }
-          <ProductValue
-            product={product}
-            key={i}
-          />
-        })}
+        {filterProducts}
       </ImageList>
     </div>
   );
