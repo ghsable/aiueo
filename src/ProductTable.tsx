5fd2440986fc58f83f0c91fdda5df438d19cfbc6
dativus <sn.sable005@gmail.com>
2022-07-02 20:22:31 +0900

    update ProductTable.tsx - add filterText

--- src/ProductTable.tsx
+++ src/ProductTable.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ProductValue from './ProductValue';
+import ImageList from '@mui/material/ImageList';
 
-function ProductTable({ products }: {products: any}) {
+function ProductTable({ products, filterText }: { products: any , filterText: string }) {
   return (
     <div className="ProductTable">
-      <ProductValue products={products} />
+      <ImageList sx={{ width: 1000, height: 850 }} cols={7} rowHeight={230}>
+        {products.map((product: any, i: number) => {
+          if (product.title.indexOf(filterText) === -1) {
+            return;
+          }
+          <ProductValue
+            product={product}
+            key={i}
+          />
+        })}
+      </ImageList>
     </div>
   );
 }
