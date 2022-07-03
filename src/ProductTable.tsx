b61dac3fa7761491076da31a7f2730d408c8fe42
dativus <sn.sable005@gmail.com>
2022-07-04 00:00:11 +0900

    update ProductTable.tsx - update any to Array<Products>

--- src/ProductTable.tsx
+++ src/ProductTable.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import ProductValue from './ProductValue';
 import ImageList from '@mui/material/ImageList';
+import { Products } from './data/PRODUCTS';
 
-function ProductTable({ products, filterText }: { products: any , filterText: string }) {
+function ProductTable({ products, filterText }: { products: Array<Products>, filterText: string }) {
   const filterProducts: any[] = [];
-  products.map((product: any, i: number) => {
+  products.map((product: Products, i: number) => {
     if (product.title.indexOf(filterText) === -1) {
       return;
     }
     filterProducts.push(
       <ProductValue
-        product={product}
-        key={i}
+        product={ product }
+        key={ i }
       />
     );
   });
 
   return (
     <div className="ProductTable">
-      <ImageList sx={{ width: 1000, height: 850 }} cols={7} rowHeight={230}>
-        {filterProducts}
+      <ImageList sx={{ width: 1000, height: 850 }} cols={ 7 } rowHeight={ 230 }>
+        { filterProducts }
       </ImageList>
     </div>
   );
