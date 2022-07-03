6828909b8a28eb272fceb24cd851c83fbc0f1bd1
dativus <sn.sable005@gmail.com>
2022-07-03 23:57:36 +0900

    update App.tsx - update any to Array<Products>

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 import SearchBar from './SearchBar';
 import ProductTable from './ProductTable';
+import { Products } from './data/PRODUCTS';
 
-function App({ products }: { products: any }) {
+function App({ products }: { products: Array<Products> }) {
   const [filterText, setFilterText] = useState('');
   const handleFilterTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFilterText(event.target.value);
-  }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <SearchBar
           filterText={ filterText }
-          handleFilterTextChange ={ handleFilterTextChange }
+          handleFilterTextChange={ handleFilterTextChange }
         />
       </header>
       <ProductTable
