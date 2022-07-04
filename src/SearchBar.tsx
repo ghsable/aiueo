ceacbfbdb7917b79d84f04e734a90aea3c3d9944
dativus <sn.sable005@gmail.com>
2022-07-04 11:47:22 +0900

    update SearchBar.tsx - remove 'component form'

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -9,7 +9,6 @@ function SearchBar({ filterText, handleFilterTextChange }: { filterText: string,
   console.log(handleFilterTextChange)
   return (
     <Paper
-      component="form"
       sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
     >
       <InputBase
